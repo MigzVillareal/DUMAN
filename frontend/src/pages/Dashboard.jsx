25cb2b94a0181454f0d87e5a273a3f54e6d927ba
Ivan Zabala <ivan.zabala011@gmail.com>
2026-06-25 17:57:29 +0800

    Add Navbar and sidebar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,12 @@
 import "../css/pages/Dashboard.css";
+import Navbar from "../components/Navbar";
 
 function Dashboard() {
-  return <div className="dashboard-page"></div>;
+  return (
+    <div className="dashboard-page">
+
+    </div>
+  );
 }
 
 export default Dashboard;
