55f3e5d2d660bddcc532394874c391c2bc386c42
cleofejianjoshua <cleofe.jianjoshua@gmail.com>
2026-07-04 01:01:59 +0800

    formatted schedule

--- api/controllers/Meeting.controller.js
+++ api/controllers/Meeting.controller.js
@@ -41,10 +41,13 @@ export const createMeeting = async (req, res) => {
             }
         });
 
+        const { format } = require('date-fns');
+        const schedule = format(new Date(meetingDate), 'EEEE, MMMM do, yyyy hh:mm a');
+        
         for (const member of members) {
             const info = await transporter.sendMail({
                 from: '"DUMAN" <duman.masaen@gmail.com>',
-                bcc: `duman.masaen@gmail.com, ${process.env.DEVS_MAIL}, ${members.memberId}`,
+                bcc: `duman.masaen@gmail.com, ${process.env.DEVS_MAIL}, ${member.memberId}`,
                 subject: `Upcoming Meeting: ${title}`,
                 text: `A meeting has been scheduled for ${schedule}.`,
             });
